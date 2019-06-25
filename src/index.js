b3335fd0a53be462940b4b0c444928cc3157cb1b
Sahanah <sahanah.ganesh@gmail.com>
2019-06-25 15:05:43 -0400

    creates actions, store, following redux tutorial

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './Modules/reducer.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './Styles/index.css';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
+  <Provider store={ store }>
   <App />,
+  </Provider>,
   document.getElementById('root')
 );
+
+export default store;
